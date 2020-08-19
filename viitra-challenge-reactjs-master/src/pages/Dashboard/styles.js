30b25a597a44a2db331470bc8d9d7ee8ce8c2eb7
danvf <dvf@cin.ufpe.br>
2020-08-19 03:32:53 -0300

    dashboard foods changed to grid display

--- viitra-challenge-reactjs-master/src/pages/Dashboard/styles.js
+++ viitra-challenge-reactjs-master/src/pages/Dashboard/styles.js
@@ -2,11 +2,10 @@ import styled from 'styled-components';
 
 export const FoodsContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: center;
+  justify-content: space-between;
+  flex-wrap: wrap;
   width: 100%;
   max-width: 1280px;
   margin: 0 auto;
-  padding: 40px 0;
   margin-top: -140px;
 `;
